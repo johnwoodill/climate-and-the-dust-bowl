a39701e00b5d318bbb50cc42a29f5dfafb058067
johnwoodill <johnwoodill@gmail.com>
2018-04-20 18:08:51 -1000

    working hybrid corn

--- hybrid_corn.R
+++ hybrid_corn.R
@@ -15,10 +15,3 @@ ggplot(NULL) + geom_line(data = hdat0, aes(year, value, group = state), color = 
   geom_label_repel(data = filter(hdat1, year == 1950), aes(year, value, label = state, color = state), segment.color = 'grey50', show.legend = FALSE, nudge_y = 10, nudge_x = -1) +
   geom_label_repel(data = filter(hdat2, year == 1940), aes(year, value, label = state, color = state),  segment.color = 'grey50', show.legend = FALSE, nudge_y = 10, nudge_x = -1)
   
-
-
-geom_text(data = filter(hdat1, year == 1960), aes(year, value, label = names(state), x = 12, colour = names(state), y = c(state), hjust = -.02))
-  
-  
-
-ggplot(hdat, aes(year, value, color = state)) + geom_line()
